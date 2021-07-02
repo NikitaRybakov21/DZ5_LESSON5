d80f0a10f4d884abe35f9e51cb12f92e34e6d586
NikitaRybakov21 <nikitarybakov044@gmail.com>
2021-07-02 16:52:40 +0300

    DZ5_LESSON5

--- src/main/java/com/github/Employee.java
+++ src/main/java/com/github/Employee.java
@@ -1,4 +1,34 @@
 package com.github;
 
 public class Employee {
+    private String FIO;
+    private String Post;
+    private String Email;
+    private String PhoneNumber;
+    private long Salary;
+    private int Age;
+
+    public Employee(String FIO, String Post, String Email, String PhoneNumber, long Salary, int Age){
+        this.FIO = FIO;
+        this.Post = Post;
+        this.Email = Email;
+        this.PhoneNumber = PhoneNumber;
+        this.Salary = Salary;
+        this.Age = Age;
+    }
+
+    public void out(){
+        System.out.print(this.FIO+" "+this.Post+" "+this.Email+" "
+                          +this.PhoneNumber+" "+this.Salary+"руб "+this.Age+"лет");
+
+    }
+
+    public int getAge(){
+        return this.Age;
+    }
+
+    public boolean getIsRetired(){
+        if(this.Age > 65) {return true;}
+        else {return false;}
+    }
 }
--- src/main/java/com/github/Main.java
+++ src/main/java/com/github/Main.java
@@ -1,4 +1,32 @@
 package com.github;
 
 public class Main {
+    public static void main(String[] args) {
+        Employee[] persArray = new Employee[5];
+        persArray[0] = new Employee("Ivanov Ivan", "Engineer", "ivivan@mailbox.com",
+                                    "892312312", 5, 11);
+        persArray[1] = new Employee("Абобуй Абоба Абобьевич", "Собиратель бананов",
+                                    "ABOBA@banan.com", "+7(777)888-00-00",
+                                    500,20);
+        persArray[2] = new Employee("Виктор Викторович Витя", "Трудовик",
+                                    "VITEK@banan.com", "+7(555)888-00-00",
+                                    3000,45);
+        persArray[3] = new Employee("Обамович Обама Абобы", "ПРЕЗИДЕНТ США",
+                                    "ОБАМА@banan.com", "+7(777)777-00-00",
+                                    20000000,40);
+        persArray[4] = new Employee("ВВП", "КРЕМЛЬ",
+                                    "ВВП@banan.com", "+7(777)777-77-77-777-777-777-777-777",
+                                    999999999999999999L,620);
+
+        int Sum = 0;
+        for (int i = 0; i < persArray.length; i++){
+            if(persArray[i].getAge() > 40) {
+                persArray[i].out();
+                Sum++;
+            }
+            if(persArray[i].getIsRetired()) System.out.println("  Пенсионер");
+            System.out.println();
+        }
+        System.out.println("Количество сотрудников "+Sum);
+    }
 }
